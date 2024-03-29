eb8065988dfcbee28aab9f8afc73cb808b930d07
Corbanator <corbandioxide42@gmail.com>
2024-03-28 21:36:19 -0600

    Implement checkpoint 1

--- core/src/com/mygdx/image_editor/ImageEditor.java
+++ core/src/com/mygdx/image_editor/ImageEditor.java
@@ -1,31 +1,46 @@
 package com.mygdx.image_editor;
 
+import java.util.Random;
+
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class ImageEditor extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-
-	@Override
-	public void create() {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render() {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+	Rec2D rectangle;
 
 	@Override
 	public void dispose() {
 		batch.dispose();
-		img.dispose();
+	}
+
+	@Override
+	public void create() {
+		batch = new SpriteBatch();
+		rectangle = new Rec2D(new Vector2(200, 100), new Vector2(200, 200), new Vector2(5, 3), Color.RED);
+	}
+
+	@Override
+	public void render() {
+		ScreenUtils.clear(0f, 0f, 0f, 1);
+		batch.begin();
+		batch.draw(rectangle.RecTexture, rectangle.Position.x, rectangle.Position.y);
+		if (rectangle.Position.x + rectangle.Scale.x >= 584 || rectangle.Position.x <= 0) {
+			rectangle.Velocity.x *= -1;
+			Random random = new Random();
+			rectangle.changeColor(new Color(random.nextFloat(), random.nextFloat(), random.nextFloat(), 1));
+		}
+		if (rectangle.Position.y + rectangle.Scale.y >= 480 || rectangle.Position.y <= 0) {
+			rectangle.Velocity.y *= -1;
+			Random random = new Random();
+			rectangle.changeColor(new Color(random.nextFloat(), random.nextFloat(), random.nextFloat(), 1));
+		}
+		rectangle.Position.add(rectangle.Velocity);
+		batch.end();
 	}
 }
--- desktop/src/com/mygdx/image_editor/DesktopLauncher.java
+++ desktop/src/com/mygdx/image_editor/DesktopLauncher.java
@@ -2,14 +2,20 @@ package com.mygdx.image_editor;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.image_editor.ImageEditor;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+
+	public static void main(String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("ImageEditor");
+		config.setWindowedMode(584, 480);
 		new Lwjgl3Application(new ImageEditor(), config);
 	}
 }
